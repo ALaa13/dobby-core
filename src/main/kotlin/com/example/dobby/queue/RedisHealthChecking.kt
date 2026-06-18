91e0893ca7f3be92218c07ffa26f3960091c6580
alaa13 <alaa.xelya@gmail.com>
2026-06-18 12:34:36 +0200

    Update Redis configuration for default values and adjust CI environment variables

--- src/main/kotlin/com/example/dobby/queue/RedisHealthChecking.kt
+++ src/main/kotlin/com/example/dobby/queue/RedisHealthChecking.kt
@@ -6,10 +6,12 @@ import org.slf4j.LoggerFactory
 import org.springframework.boot.CommandLineRunner
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.data.redis.connection.RedisConnectionFactory
 import kotlin.system.exitProcess
 
 @Configuration
+@Profile("!test")
 class RedisHealthCheckConfig {
 
     @Bean
