c6635abd84d0d070efb74f584d770eecbadb7341
alaa13 <alaa.xelya@gmail.com>
2026-09-10 21:07:59 +0200

    Added PostgreSQL to docker compose and updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,6 +43,8 @@ dependencies {
     implementation("com.auth0:java-jwt:4.4.0")
     implementation("org.springframework.boot:spring-boot-starter-security")
 
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("org.postgresql:postgresql")
     implementation("io.github.jan-tennert.supabase:postgrest-kt:$supabaseVersion")
     implementation("io.github.jan-tennert.supabase:auth-kt:$supabaseVersion")
     implementation("io.github.jan-tennert.supabase:storage-kt:$supabaseVersion")
